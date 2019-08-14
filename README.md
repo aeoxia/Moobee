15672172f530fc18fdec9397cb996dfa352e60af
Jan Kennu Paz <38674945+aeoxia@users.noreply.github.com>
2019-08-14 20:03:47 +0800

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Moobee
+![picture alt](https://i.imgur.com/9mFMWrj.png?1 "Moobee") A simple project created for the purposes of exploring new Android development tools focusing mainly on architecture components together with the tools and approaches which I have been using for multiple projects. The following are the highlights of the project:
+
+* Clean architecture
+* Dagger2
+* Retrofit
+* MVVM with Databinding, Live Data and Android ViewModels
+* Coroutines
+* Navigation architecture component
+* AndroidX package
--- presentation/src/main/java/com/ausom/moobee/MoobeeApplication.kt
+++ presentation/src/main/java/com/ausom/moobee/MoobeeApplication.kt
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Jan Kennu Paz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.ausom.moobee
 
 import android.app.Activity
@@ -6,6 +30,8 @@ import com.ausom.moobee.di.AppInjector
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.ObsoleteCoroutinesApi
 import javax.inject.Inject
 
 class MoobeeApplication: Application(), HasActivityInjector{
@@ -15,6 +41,8 @@ class MoobeeApplication: Application(), HasActivityInjector{
 
     override fun activityInjector(): AndroidInjector<Activity> = activityInjector
 
+    @ObsoleteCoroutinesApi
+    @ExperimentalCoroutinesApi
     override fun onCreate() {
         super.onCreate()
         AppInjector.init(this)
